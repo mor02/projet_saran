67807e2fb2c1d995757445ca3ebfc66ed9c575b6
azur22 <58863062+azur22@users.noreply.github.com>
2019-12-20 13:46:32 +0100

    Update ConstanteMessage.java

--- src/com/poo/mime/messages/ConstanteMessage.java
+++ src/com/poo/mime/messages/ConstanteMessage.java
@@ -3,6 +3,6 @@ package com.poo.mime.messages;
 public class ConstanteMessage {
 
 	public static final String MESSAGE_ANOMALIE_FICHIER_VIDE="Fichier vide";
-	public static final String MESSAGE_ANOMALIE_FICHIER_EXTENTION_DIFF_MIME=".";
-	public static final String MESSAGE_ANOMALIE_REP_NON_TROUVE="RÈpertoire non trouvÈ";
+	public static final String MESSAGE_ANOMALIE_FICHIER_EXTENTION_DIFF_MIME="ce fichier pr√©sente une anomalie car l'extension est differente du MIME";
+	public static final String MESSAGE_ANOMALIE_REP_NON_TROUVE="R√©pertoire non trouv√©";
 }
