377169aa3f2936e7706ab2fdf8e94fb7cbf5dd89
azur22 <58863062+azur22@users.noreply.github.com>
2019-12-26 21:55:15 +0100

    Update ExceptionExtensionEnPlusieurs.java

--- src/com/poo/mime/exceptions/ExceptionExtensionEnPlusieurs.java
+++ src/com/poo/mime/exceptions/ExceptionExtensionEnPlusieurs.java
@@ -4,8 +4,11 @@ public class ExceptionExtensionEnPlusieurs extends Exception {
 
 	private String message;
 	
+	public ExceptionExtensionEnPlusieurs (String mesg){
+		this.message=mesg;
+	}
 	public void setMessage(String mesg) {
-		this.message = mesg;
+		this.message = mes;
 	}
 	
 	public String getMessage() {
